52c4bae79685c2a3225bb97d92f0ef390685d016
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-02 20:33:15 +0300

    ex2

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-#теорія декоратори
-
-def my_decorator_func(func): #3
-    def wrapper(): #4
-        print('lalalala')
-        func()
-        print('okeokeoke')
-    return wrapper
-
-
-@my_decorator_func #2
-def say_hello(): #1
-    print('Hello!')
-say_hello()
-
 import time
-def delay_decorator(func):
+
+
+def time_function(func):
     def wrapper(*args, **kwargs):
-        time.sleep(0.1)
-        return func(*args, **kwargs)
+        stat_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print('Час виконання функції', end_time - stat_time)
+        return result
     return wrapper
 
-@delay_decorator
-def sleepy():
-    print('splyu(((')
-sleepy()
-
-#3
-def cache_decorator(func):
-    cache = {}
-    def wrapper(n):
-        if n not in cache:
-            cache[n] = func(n)
-        return cache[n], cache
-    return wrapper
-@cache_decorator
-def fibonacci(n):
-    if n in(0, 1):
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
-print(fibonacci(10))
+@time_function
+def example_function(x):
+    time.sleep(1) #pass
+    return x
+print(example_function(666))

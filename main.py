d33b483c20bb65cb0373a71357e421c6335be6d8
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-02 18:55:39 +0300

    second exemplal

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def say_hello(): #1
     print('Hello!')
 say_hello()
 
+import time
+def delay_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+
+@delay_decorator
+def sleepy():
+    print('splyu(((')
+sleepy()
